2eb3cff15b08a884347dce0fe3135a349dfaf672
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 15:46:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1 
      array 
   elsif array.length == 2 
-   array.join ("," , "and")
+   array.join ("," "and")
 else array.length > 2 
    array.join (",") << "and"
  end 
