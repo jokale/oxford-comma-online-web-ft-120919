e9c5fb5e6d7f20480a96fe8d442607a2ec322562
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 15:50:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
     array 
   elsif array.length == 2 
   array.join (", and")
-  elsif array.length <
+  elsif array.length > 2 
+  
 end 
 # def oxford_comma(array)
 # array.join(",")
