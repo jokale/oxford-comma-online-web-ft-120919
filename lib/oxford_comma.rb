c9780eb723e28db2641d9eef10910fe74bfaf58e
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 15:47:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,20 @@
+# def oxford_comma(array)
+#   if array.length == 1 
+#     array 
+#   elsif array.length == 2 
+#   array.join ("," "and")
+# else array.length > 2 
+#   array.join (",") << "and"
+# end 
+# end 
+# array 
+
+# end 
 def oxford_comma(array)
-  if array.length == 1 
-     array 
-  elsif array.length == 2 
-   array.join ("," "and")
-else array.length > 2 
-   array.join (",") << "and"
- end 
- end 
- array 
+array.join(",")
+end
 
- end 
-
+def oxford_comma(array)
+  array.join(" and ")
+end
 
