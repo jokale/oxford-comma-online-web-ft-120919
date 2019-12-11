70e7b7fb5546671136909b5ad03a2967d6612132
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 15:48:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,22 +1,22 @@
+def oxford_comma(array)
+  if array.length == 1 
+    array 
+  elsif array.length == 2 
+  array.join (", and")
+else array.length > 2 
+  array.join (",") << "and"
+end 
+end 
+array 
+
+end 
 # def oxford_comma(array)
-#   if array.length == 1 
-#     array 
-#   elsif array.length == 2 
-#   array.join (", and")
-# else array.length > 2 
-#   array.join (",") << "and"
-# end 
-# end 
-# array 
+# array.join(",")
+# end
 
-# end 
-def oxford_comma(array)
-array.join(",")
-end
-
-def oxford_comma(array)
-  array.join(" and ")
-end
+# def oxford_comma(array)
+#   array.join(" and ")
+# end
 
 
 
