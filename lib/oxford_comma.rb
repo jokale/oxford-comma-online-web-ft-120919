f169fb43c948b0516a266853a0da04e8ad711c16
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 15:59:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,9 @@ def oxford_comma(array)
   elsif array.length == 2 
   array.join (" and ")
   elsif array.length >= 3
-  array.join (", and")
-else array.length >
+
+
+
   
 end 
 # def oxford_comma(array)
